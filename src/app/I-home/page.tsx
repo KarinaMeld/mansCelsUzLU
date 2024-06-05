ac8d4d47091a47d357731a7b808689ae972fba95
Karīna Meldere <meld.karina@gmail.com>
2024-06-05 14:03:48 +0300

    First page content added

--- src/app/I-home/page.tsx
+++ src/app/I-home/page.tsx
@@ -1,53 +1,90 @@
 import Head from "next/head";
 import Image from "next/image";
-import pages from "../scss/pages.module.scss";
+import "../scss/pages.scss";
+import Link from "next/link";
+import kitten from "@/app/images/kittens-bloda.png";
+import arrow5 from "@/app/images/Arrow5.svg";
+import arrow6 from "@/app/images/Arrow6.svg";
 
 export default function Home() {
   return (
-    <div className={pages.container}>
-      <div className={pages.heading}>
-        <title>mājas</title>
-        <div className={pages.heading_line}></div>
+    <div className="container">
+      <div className="heading-div">
+        <h1 className="">mājas, mīļās mājas</h1>
       </div>
 
-      <main className={pages.main}>
-        <h1 className={pages.title}>starppilsētu autobuss</h1>
-
-        <div className={pages.descriptionContainer}>
-          <div className={pages.imageContainer}>
-            <Image
-              src="/path-to-image1.jpg"
-              alt="Image 1"
-              width={300}
-              height={200}
-            />
+      <div className="rest-of-content">
+        <div className="row row-one">
+          <div className="col-7">
+            <p className="step-info">
+              katrs mans rīts sākas "mājās". kā es aprakstītu savas mājas?
+              haoss. tīrākais haoss. šogad šo haosu vēl vairāk saasināja divu
+              identiski pelēku kaķēnu uzrašanās.
+            </p>
+            <p>
+              par viņu darbībām visvairāk informēta ir manas mammas mīļākā puķe.
+              viņu dēļ tā bijusi spiesta mainīt puķu podu un atrašanās vietu nu
+              jau trīs reizes (balstoties uz man piejamo informāciju
+              05.06.2024.).
+            </p>
+            <p id="img-description">Attēls: "kaķēni bļodā", Vangaži, 2024.</p>
           </div>
-          <p className={pages.description}>
-            garāks posma apraksts - no kurienes uz kurieni tieši maršruta posms
-            ved, kā arī aprakstīts tajā izmantotais transports
-          </p>
+          <div className="col-5">
+            <div className="vertical-line"></div>
+            <div className="image d-flex justify-content-center">
+              <Image
+                src={kitten}
+                alt="Picture of kittens"
+                className="image img-fluid image-one"
+              />
+            </div>
+          </div>
         </div>
 
-        <div className={pages.descriptionContainer}>
-          <div className={pages.imageContainer}>
-            <Image
-              src="/path-to-image2.jpg"
-              alt="Image 2"
-              width={300}
-              height={200}
-            />
+        <div className="row row-two">
+          <div className="board-game-info col-7">
+            <h3 className="board-game-heading">
+              <i>Exploding kittens</i>
+            </h3>
+            <p>this is a longer explanation about the board game</p>
+            <p id="img-description">
+              Attēls: "Exploding kittens board game", Jūrmala, 2024.
+            </p>
           </div>
-          <p className={pages.description}>
-            aprakstīta posmam izvēlētā galda spēle, tās noteikumi un kā tieši tā
-            ir līdzīga attiecīgajam starpposmam.
-          </p>
+          <div className="col-5">
+            <div className="vertical-line"></div>
+            <div className="image d-flex justify-content-center">
+              <Image
+                src={kitten}
+                alt="Picture of the board game 'Exploding Kittens'"
+                className="image img-fluid image-two"
+              />
+            </div>
+          </div>
         </div>
-      </main>
-
-      <footer className={pages.footer}>
-        <div className={pages.arrowLeft}></div>
-        <div className={pages.arrowRight}></div>
-      </footer>
+        <div className="navigation-buttons row">
+          <div className="col-7">
+            <Image
+              src={arrow5}
+              alt="Arrow SVG"
+              className="arrow arrow-one img-fluid"
+            />
+            <div className="nav-but button-prev">
+              <Link href={"/"} className="link"></Link>
+            </div>
+          </div>
+          <div className="col-5">
+            <Image
+              src={arrow6}
+              alt="Arrow SVG"
+              className="arrow arrow-two img-fluid"
+            />
+            <div className="nav-but button-next">
+              <Link href={"/"} className="link"></Link>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
